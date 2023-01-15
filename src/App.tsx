977aa6f730002dcdabdc87dd9ea6ed7853ade628
Austin Cai <austin@lumosidentity.com>
2023-01-15 13:34:49 -0800

    reformat App using Prettier formatter

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
-import { createTheme, Link, List, ListItem, Stack, ThemeProvider, Typography } from '@mui/material'
-import headshot from './images/austin_headshot.jpeg'
-import { Footer } from './Footer';
-import Terminal from './fonts/SvBasicManual.ttf';
+import React from "react";
+import {
+  createTheme,
+  Link,
+  List,
+  ListItem,
+  Stack,
+  ThemeProvider,
+  Typography,
+} from "@mui/material";
+import headshot from "./images/austin_headshot.jpeg";
+import { Footer } from "./Footer";
+import Terminal from "./fonts/SvBasicManual.ttf";
 
 // TODO: fix fonts
 const theme = createTheme({
   typography: {
-    fontFamily: 'Terminal, Arial',
+    fontFamily: "Terminal, Arial",
   },
   components: {
     MuiCssBaseline: {
@@ -25,28 +33,63 @@ const theme = createTheme({
 });
 
 function App() {
-  const imageHeight = 150
+  const imageHeight = 150;
 
   return (
     <ThemeProvider theme={theme}>
-      <Stack height='95vh' m={2} flexDirection="column" justifyContent="space-between">
-        <Stack className='homepage-content' flexDirection="row" justifyContent="space-between">
+      <Stack
+        height="95vh"
+        m={2}
+        flexDirection="column"
+        justifyContent="space-between"
+      >
+        <Stack
+          className="homepage-content"
+          flexDirection="row"
+          justifyContent="space-between"
+        >
           <Stack>
-            <Typography variant='h2'>Hi, I'm Austin!</Typography>
-            <Typography >Welcome to my website! This is a work in progress, but in the meantime here are some links you might find interesting:</Typography>
+            <Typography variant="h2">Hi, I'm Austin!</Typography>
+            <Typography>
+              Welcome to my website! This is a work in progress, but in the
+              meantime here are some links you might find interesting:
+            </Typography>
             <List>
-              <ListItem><Link underline="hover" target="_blank" href="https://austincai.notion.site/Projects-83945388f6ac4b21b5fde4e7b601ebf3"><Typography>Projects</Typography></Link></ListItem>
-              <ListItem><Link underline="hover" target="_blank" href="http://austincai.weebly.com/"><Typography>Art</Typography></Link></ListItem>
+              <ListItem>
+                <Link
+                  underline="hover"
+                  target="_blank"
+                  href="https://austincai.notion.site/Projects-83945388f6ac4b21b5fde4e7b601ebf3"
+                >
+                  <Typography>Projects</Typography>
+                </Link>
+              </ListItem>
+              <ListItem>
+                <Link
+                  underline="hover"
+                  target="_blank"
+                  href="http://austincai.weebly.com/"
+                >
+                  <Typography>Art</Typography>
+                </Link>
+              </ListItem>
             </List>
           </Stack>
           <Stack>
-            <img src={headshot} alt={"Headshot of Austin"} style={{width: imageHeight, height: imageHeight, borderRadius: imageHeight/2}}></img>
+            <img
+              src={headshot}
+              alt={"Headshot of Austin"}
+              style={{
+                width: imageHeight,
+                height: imageHeight,
+                borderRadius: imageHeight / 2,
+              }}
+            ></img>
           </Stack>
         </Stack>
-        <Footer/>
+        <Footer />
       </Stack>
     </ThemeProvider>
-
   );
 }
 
