662f01ceb37060c8b857e97d3d7e7054b4c5c69f
Austin Cai <austin@lumosidentity.com>
2023-01-15 13:30:59 -0800

    add links to project, art pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createTheme, Stack, ThemeProvider, Typography } from '@mui/material'
+import { createTheme, Link, List, ListItem, Stack, ThemeProvider, Typography } from '@mui/material'
 import headshot from './images/austin_headshot.jpeg'
 import { Footer } from './Footer';
 import Terminal from './fonts/SvBasicManual.ttf';
@@ -33,7 +33,11 @@ function App() {
         <Stack className='homepage-content' flexDirection="row" justifyContent="space-between">
           <Stack>
             <Typography variant='h2'>Hi, I'm Austin!</Typography>
-            <Typography >Welcome to my website!</Typography>
+            <Typography >Welcome to my website! This is a work in progress, but in the meantime here are some links you might find interesting:</Typography>
+            <List>
+              <ListItem><Link underline="hover" target="_blank" href="https://austincai.notion.site/Projects-83945388f6ac4b21b5fde4e7b601ebf3"><Typography>Projects</Typography></Link></ListItem>
+              <ListItem><Link underline="hover" target="_blank" href="http://austincai.weebly.com/"><Typography>Art</Typography></Link></ListItem>
+            </List>
           </Stack>
           <Stack>
             <img src={headshot} alt={"Headshot of Austin"} style={{width: imageHeight, height: imageHeight, borderRadius: imageHeight/2}}></img>
